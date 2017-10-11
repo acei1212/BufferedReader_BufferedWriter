b4d8aaf01a8996887b589bc0e2ab69a9181c90d4
acei <acei1212@gmail.com>
2017-10-11 13:12:54 +0800

    新增 存取至sd卡，並讀取

--- app/src/main/java/com/example/student/myapplication/MainActivity.java
+++ app/src/main/java/com/example/student/myapplication/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.student.myapplication;
 
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Environment;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -15,7 +19,11 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 
+import static android.Manifest.permission.READ_EXTERNAL_STORAGE;
+import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
+
 public class MainActivity extends AppCompatActivity {
+    final int REQUEST_EXTERNAL_STORAGE = 321;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -138,6 +146,97 @@ public class MainActivity extends AppCompatActivity {
             Log.d("FNAME", "data:" + i);
         }
 
+    }
+
+    public void clickWriter4(View v ){
+        String path = getFilesDir().getAbsolutePath();
+        File fname = new File(path + File.separator + "data3.txt");
+        try {
+            FileWriter fw = new FileWriter(fname);
+            BufferedWriter bw = new BufferedWriter(fw);
+            bw.write("Hello,world");
+            bw.newLine();
+            bw.write("This is android");
+            bw.close();
+            fw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public void clickReader4(View v){
+
+    }
+
+    public void clickWriter5(View v)
+    {
+        int permission = ActivityCompat.checkSelfPermission(this,
+                WRITE_EXTERNAL_STORAGE);
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            //未取得權限，向使用者要求允許權
+            ActivityCompat.requestPermissions(this, new String[]{WRITE_EXTERNAL_STORAGE, READ_EXTERNAL_STORAGE},
+                    REQUEST_EXTERNAL_STORAGE);
+        }else{
+            //已有權限，可進行檔案存取
+            writeFile();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_EXTERNAL_STORAGE)
+        {
+            if (grantResults.length > 0
+                    && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                //取得權限，進行檔案存取
+                writeFile();
+            } else {
+                //使用者拒絕權限，停用檔案存取功能
+            }
+        }
+    }
+
+    private void writeFile()
+    {
+        File f = Environment.getExternalStorageDirectory();
+        Log.d("FNAME", f.getAbsolutePath());
+        File f2 = new File(f.getAbsolutePath() + File.separator + "mydata");
+        f2.mkdir();
+        File txtFile = new File(f2.getAbsolutePath() + File.separator + "data5.txt");
+        try {
+            FileWriter fw = new FileWriter(txtFile);
+            BufferedWriter bw = new BufferedWriter(fw);
+            bw.write("Hello World");
+            bw.newLine();
+            bw.write("This is android");
+            bw.close();
+            fw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public void clickReader5(View v)
+    {
+        File f = Environment.getExternalStorageDirectory();
+        Log.d("FNAME", f.getAbsolutePath());
+        File f2 = new File(f.getAbsolutePath() + File.separator + "mydata");
+        File txtFile = new File(f2.getAbsolutePath() + File.separator + "data5.txt");
+        try {
+            FileReader fr = new FileReader(txtFile);
+            BufferedReader br = new BufferedReader(fr);
+            String str;
+            while ((str=br.readLine()) != null)
+            {
+                Log.d("FNAME", "Read:" + str);
+            }
+            br.close();
+            fr.close();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
